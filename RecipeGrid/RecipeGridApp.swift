9ba237bd86fad57b3ec366fbb687d1352cdb8414
lajsan <lajsanminlebaeva@mail.ru>
2026-01-27 08:43:38 -0800

    Initial Commit

---
+++ RecipeGrid/RecipeGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeGridApp.swift
+//  RecipeGrid
+//
+//  Created by Ляйсан on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
